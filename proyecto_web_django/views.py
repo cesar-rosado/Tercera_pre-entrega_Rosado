f4f474ae0e81f0fd13f49a42aebb4ed11fb78431
César Ismael Rosado Villanueva <cirv84@hotmail.com>
2023-05-10 23:58:09 -0500

    1-Creando la carpeta templates y subcarpeta catalogo. 2-Se creo el archivo base.html. 3- Realizando prueba con funcion saludar_con-html

--- proyecto_web_django/views.py
+++ proyecto_web_django/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def saludar(request):
+def saludar(request): #Funcion de prueba
     saludo = "Hola usuario bienvenido a la pagina de prueba"
     pagina_html = HttpResponse(saludo)
     return pagina_html
+
+def saludar_con_html(request): #Funcion de prueba
+    contexto = {}
+    http_response = render(
+        request=request,
+        template_name='catalogo/base.html',
+        context=contexto,
+    )
+    return http_response
