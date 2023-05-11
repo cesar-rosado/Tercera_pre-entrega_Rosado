d9967c6f5a7f2ec7a78f7dc7bb06effc2956331c
César Ismael Rosado Villanueva <cirv84@hotmail.com>
2023-05-11 17:31:21 -0500

    Activando el panel administrativo e ingresando datos a la base de datos

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from catalogo.models import Cliente, Computadora, Accesorio, Proveedor
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Computadora)
+admin.site.register(Accesorio)
+admin.site.register(Proveedor)
--- catalogo/models.py
+++ catalogo/models.py
@@ -4,25 +4,37 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=256)
     apellido = models.CharField(max_length=256)
     email = models.EmailField()
-    fecha_nacimiento = models.DateField()
-    telefono = models.IntegerField()
-    direccion= models.CharField(max_length=256)
+    fecha_nacimiento = models.DateField(blank=True)
+    telefono = models.IntegerField(blank=True)
+    direccion= models.CharField(max_length=256, blank=True)
+    
+    def __str__(self):
+        return f"{self.nombre}  {self.apellido}"
     
 class Computadora(models.Model):
     tipo_computadora = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
     procesador = models.CharField(max_length=20)
-    ram = models.IntegerField()
-    hhd = models.IntegerField()
+    ram = models.IntegerField(blank=True)
+    hhd = models.IntegerField(blank=True)
+    
+    def __str__(self):
+        return f"{self.tipo_computadora} | {self.marca} | {self.modelo}"
     
 class Accesorio(models.Model):
     tipo_accesorio = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f"{self.tipo_accesorio} | {self.marca} | {self.modelo}"
 
 class Proveedor(models.Model):
     empresa = models.CharField(max_length=256)
-    direccion = models.CharField(max_length=256)
+    direccion = models.CharField(max_length=256, blank=True)
     email = models.EmailField()
-    telefono = models.IntegerField()
+    telefono = models.IntegerField(blank=True)
+    
+    def __str__(self):
+        return f"{self.empresa}"
